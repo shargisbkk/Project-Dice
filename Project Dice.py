a9d6dff30b1d920b31a6273ed72bffcd4f3d3993
shargisbkk <shargis664@gmail.com>
2024-04-24 02:12:52 -0400

    done command

--- Project Dice.py
+++ Project Dice.py
@@ -20,6 +20,7 @@ num_sides = 6  # Set the number of sides on each die to 6
 while True:
     command = input("Enter 'roll' to roll dice, 'reroll' to reroll, or 'exit' to exit: ")
     
+    #roll command for initial dice roll
     if command == 'roll':
         rolls = roll_dice(num_dice, num_sides)
         print(f"Rolling {num_dice} dice with {num_sides} sides each:")
@@ -29,6 +30,7 @@ while True:
         store = store_roll(rolls, store, indices)
         print("Total stored rolls:", store)
     
+    #set rerolls to maximum 3
     elif command == 'reroll':
         new_rolls = reroll(store, num_dice, num_sides)
         print(f"Rerolling {num_dice - len(store)} dice with {num_sides} sides each:")
@@ -38,6 +40,10 @@ while True:
         store = store_roll(new_rolls, store, indices)
         print("Total stored rolls:", store)
     
+    #finalize all dice
+    elif command == 'done':
+        print("Final Score", store)
+
     elif command == 'exit':
         break
     
